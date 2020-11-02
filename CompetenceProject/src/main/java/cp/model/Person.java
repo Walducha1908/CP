edb59ff0825fa2aab502623c43b101a003488a6a
Bartłomiej Jencz <bartek.jencz@wp.pl>
2020-11-02 22:34:41 +0100

    Added simple example JSON for person

--- CompetenceProject/src/main/java/cp/model/Person.java
+++ CompetenceProject/src/main/java/cp/model/Person.java
@@ -8,7 +8,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
-
+/*
+Example JSON
+{
+    "phoneNumber": "test2",
+    "profile": "students",
+}
+ */
 @Document(collection = "Persons")
 @Builder
 @AllArgsConstructor
