96f2c780ff12d9a2ec2fd85b0abfb487ab3a8d18
Bartłomiej Jencz <bartek.jencz@wp.pl>
2020-11-02 22:14:33 +0100

    Added working hello service

---
+++ CompetenceProject/src/main/java/cp/controlers/HelloControler.java
@@ -0,0 +1,17 @@
+package cp.controlers;
+
+import cp.services.HelloService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloControler {
+    private final HelloService helloService;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return helloService.sayHello();
+    }
+}
