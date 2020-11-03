1d8551af837c5815899bc9f3196e6325b8fd42c4
Bartłomiej Jencz <bartek.jencz@wp.pl>
2020-11-03 12:51:23 +0100

    Added simulation service and controller

---
+++ CompetenceProject/src/main/java/cp/services/SimulationService.java
@@ -0,0 +1,19 @@
+package cp.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class SimulationService {
+
+    /**
+     * Method responsible for running simulation.
+     * @throws Exception
+     */
+    public void runSimulation() throws Exception {
+        throw new Exception("Simulation not implemented");
+    }
+}
